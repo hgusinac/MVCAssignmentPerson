0e243f16295d6453ed56bda420bd16de9ede96f8
Haris Gusinac <hgusinac@gmail.com>
2021-05-04 09:33:21 +0200

    database works. CRUD

--- Database/DatabasePeopleRepo.cs
+++ Database/DatabasePeopleRepo.cs
@@ -16,47 +16,83 @@ namespace MVCAssignmentPerson.Database
             this.peopleDbContext = peopleDbContext;
         }
 
-        public Person Create(CreatePersonViewModel createPerson)//Denna krånglar----CreatePersonViewModel createPerson)
+        public Person Create(CreatePersonViewModel createPerson)
         {
-            Person person = new Person();
-            peopleDbContext.Add(person);
-            int result = peopleDbContext.SaveChanges();
+            Person person = new Person();// skapar en person 
 
-            if(result == 0)
+            person.Name = createPerson.Name; // överför datan från createPerson till Person. 
+            person.City = createPerson.City;
+            person.Phone = createPerson.Phone;
+
+            peopleDbContext.Add(person);// lägger till person med rätt namn city phone.
+            int result = peopleDbContext.SaveChanges();//sparar ändringar. 
+
+            if (result == 0)
             {
                 throw new Exception("Unable to add a Person to database");
             }
             return person;
         }
 
-     
 
-        public bool Delete(int id)
-        {
 
-            //throw new NotImplementedException();
-
-            peopleDbContext.Remove(id);
-            if(this.Read(id) == null)
-            {
-                return true;
-            }
-            return false;
-        }
+       
         public Person Read(int id)
         {
-           return peopleDbContext.people.SingleOrDefault(row => row.Id == id);
+            return peopleDbContext.people.SingleOrDefault(row => row.Id == id);
         }
 
         public List<Person> Read()
         {
             return peopleDbContext.people.ToList();
         }
-
-
         public Person Update(Person person)
         {
-            throw new NotImplementedException();
+            Person OriginalPerson = Read(person.Id);
+
+            if (OriginalPerson == null)
+            {
+                return null;
+
+            }
+
+            OriginalPerson.Name = person.Name;
+            OriginalPerson.Phone = person.Phone;
+            OriginalPerson.City = person.City;
+
+            int result = peopleDbContext.SaveChanges();
+
+            if (result == 0)
+            {
+                throw new Exception("Unable to add a Person to database");
+            }
+
+
+            return person;
+        }
+
+
+
+        public bool Delete(int id)
+        {
+
+            //throw new NotImplementedException();
+            Person person = Read(id);//hittar person med id
+            if (person == null)
+            {
+                return false;
+            }
+
+            peopleDbContext.Remove(person);// raderar personen 
+
+            int result = peopleDbContext.SaveChanges();// sparar förändringen
+
+            if (result == 0)// om ingen förändring sker misslyckas raderingen.
+            {
+                return false;
+            }
+
+            return true;
         }
     }
 }
