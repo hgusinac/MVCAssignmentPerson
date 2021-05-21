88d58e0c889d9c6c63eed64ddc99fb48e8f4c3dc
Haris Gusinac <hgusinac@gmail.com>
2021-05-21 11:11:04 +0200

    add Seed.

---
+++ Database/DbInitializer.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCAssignmentPerson.Database
+{
+    public class DbInitializer
+    {
+        public static void Initialize(
+          PeopleDbContext context, //Database
+          RoleManager<IdentityRole> roleManager, //Role
+          UserManager<AppUser> AppUser)//User
+        {
+            //context.Database.EnsureCreated();//If not using EF migrations
+            context.Database.Migrate();
+
+
+            if (context.Roles.Any())//letar efter roller i databasen
+            {
+                return;//
+            }
+
+            //----------Seed into database --------------------------------------
+
+
+
+            string[] rolesToSeed = new string[] { "Admin", "User" };
+
+            foreach (var roleName in rolesToSeed)
+            {
+
+                IdentityRole role = new IdentityRole(roleName);
+
+                var result = roleManager.CreateAsync(role).Result;
+
+                if (!result.Succeeded)
+                {
+                    throw new Exception("Faild to create Role" + roleName);
+                }
+            }
+
+            AppUser user = new AppUser()
+            {
+                UserName = "AdminPower",
+                FirstName = "Admin",
+                LastName = "Admin",
+                BirthDate= "2020",
+                Email = "a@a.a",
+                PhoneNumber = "123456789"
+            };
+            IdentityResult resultUser = AppUser.CreateAsync(user, "Qwer1234!").Result;
+
+            if (!resultUser.Succeeded)
+            {
+                throw new Exception("Faild to create Admin Acc: AdminPower");
+            }
+
+            IdentityResult resultAssign = AppUser.AddToRoleAsync(user, rolesToSeed[0]).Result;
+
+            if (!resultAssign.Succeeded)
+            {
+                throw new Exception($"Faild to grant Admin {rolesToSeed[0]} to AdminPower");
+            }
+        }
+    }
+}
+    
+
--- Program.cs
+++ Program.cs
@@ -1,7 +1,10 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using MVCAssignmentPerson.Database;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,7 +16,11 @@ namespace MVCAssignmentPerson
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            CreateDbIfNotExists(host);
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -22,5 +29,31 @@ namespace MVCAssignmentPerson
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
+
+        private static void CreateDbIfNotExists(IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<PeopleDbContext>();
+                    RoleManager<IdentityRole> roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+                    UserManager<AppUser> AppUser = services.GetRequiredService<UserManager<AppUser>>();
+
+                    DbInitializer.Initialize(context, roleManager, AppUser);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while creating the DB.");
+
+
+
+                }
+            }
+        }
     }
 }
+
